0572250beef0f32eebbc7c4bb2df6cf39321cc67
lelouvincx <dinhminhchinh3357@gmail.com>
2022-12-18 00:25:43 +0700

    Change to t2.medium

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -16,7 +16,7 @@ variable "key_name" {
 variable "instance_type" {
   description = "Instance type for EC2"
   type        = string
-  default     = "t2.micro"
+  default     = "t2.medium"
 }
 
 ## S3
